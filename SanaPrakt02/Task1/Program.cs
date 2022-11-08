304615c748d5c0c95930955ba84a1df949933553
Andrii Nemerovskii <andreynemerovskiy@gmail.com>
2022-11-09 00:29:41 +0200

    Change first 3 lines

--- SanaPrakt02/Task1/Program.cs
+++ SanaPrakt02/Task1/Program.cs
@@ -1,6 +1,6 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Курси Commerce Software Development Bootcamp");
-Console.WriteLine("Курси Commerce Software Development Bootcamp");
-Console.WriteLine("Курси Commerce Software Development Bootcamp");
+Console.WriteLine("Курси");
+Console.WriteLine("Hi");
+Console.WriteLine("Hello");
 Console.WriteLine("Курси Commerce Software Development Bootcamp");
 Console.WriteLine("Курси Commerce Software Development Bootcamp");
