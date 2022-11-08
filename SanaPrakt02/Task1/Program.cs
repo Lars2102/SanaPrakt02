5fec5ba04754bb35594f7eea6679212003dfe0b0
Andrii Nemerovskii <andreynemerovskiy@gmail.com>
2022-11-09 00:27:21 +0200

    Add 4 text line

--- SanaPrakt02/Task1/Program.cs
+++ SanaPrakt02/Task1/Program.cs
@@ -1,3 +1,6 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Курси Commerce Software Development Bootcamp");
-
+Console.WriteLine("Курси Commerce Software Development Bootcamp");
+Console.WriteLine("Курси Commerce Software Development Bootcamp");
+Console.WriteLine("Курси Commerce Software Development Bootcamp");
+Console.WriteLine("Курси Commerce Software Development Bootcamp");
